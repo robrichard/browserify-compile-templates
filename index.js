6b2fcfdb5f8582d594eeeadbaf88bee9ae06a732
Rob Richard <rob.richard@1stdibs.com>
2014-03-20 16:50:31 -0400

    use replace extension, take a config instead of hardcoding

--- index.js
+++ index.js
@@ -1,30 +1,25 @@
+"use strict";
+
 var compileFile = require('./lib/compile.js');
 var fs = require('graceful-fs');
 var _ = require('underscore');
 var file = require('file');
+var replaceExt = require('replace-ext');
 
-
-var walk = function (options) {
+module.exports = function (options) {
     _.each(options, function (option) {
         file.walk(option.input, function (nill, dirPath, dirs, files) {
-//            console.log(option.input);
             compileDirectory(option.input, option.output, files);
         });
     });
-}
+
+
+};
 
 var compileDirectory = function (inputBaseDir, outputBaseDir, files) {
     _.each(files, function (file) {
         var relativePath = _.last(file.split(inputBaseDir));
-        var newFile = outputBaseDir + '/' + _.initial(relativePath.split('.')) + '.js';
+        var newFile = outputBaseDir + '/' + replaceExt(relativePath, '.js') + '.js';
         compileFile(file, newFile);
     });
-}
-
-walk([
-    {
-        input: '/Users/Richard/projects/1stdibs-admin-v2/public/templates',
-        output: '/Users/Richard/projects/1stdibs-admin-v2/src/js/templates'
-    }
-]);
-
+};
