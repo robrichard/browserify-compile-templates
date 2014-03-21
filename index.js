80d0368dead31b3b7a52275bafcd37369ebd9bdb
Rob Richard <rob.richard@1stdibs.com>
2014-03-21 10:19:23 -0400

    Take options to disable verbose logging

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 var compileFile = require('./lib/compile.js');
 var path = require('path');
-
+var _ = require('underscore');
 var through = require('through2');
 var gutil = require('gulp-util');
 var PluginError = gutil.PluginError;
 
 const PLUGIN_NAME = 'gulp-compile-templates';
 
-function gulpCompiler() {
+function gulpCompiler(passedOptions) {
+    var options = _.defaults(passedOptions || {}, {
+            verbose: true
+        });
+
     var stream = through.obj(function (file, enc, callback) {
         var output;
         var dest = gutil.replaceExtension(file.path, ".js");
@@ -22,6 +26,9 @@ function gulpCompiler() {
             file.contents = new Buffer(output, "utf-8");
             file.path = dest;
             this.push(file);
+            if (options.verbose) {
+                gutil.log('Compiled template:', file.path);
+            }
             return callback();
         }
 
